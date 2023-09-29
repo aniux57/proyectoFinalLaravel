6b8d00d0251ba6d0466946489ecd3ebcc086eb12
keithre19 <ivana2003@umes.edu.gt>
2023-09-29 13:29:00 -0600

    FH-93 Migracion y Modelo Laboratorio

---
+++ app/Models/Laboratorio.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Laboratorio extends Model
+{
+    use HasFactory;
+
+    //atributos a llenar o acceder en la base de datos
+    protected $fillable = ['nombre', 'descripcion', 'estado'];
+}
+?>
