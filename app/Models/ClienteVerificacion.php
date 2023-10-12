69385d9226a11493c2a83406154e3ab4abc3fb31
keithre19 <ivana2003@umes.edu.gt>
2023-10-11 23:07:36 -0600

    agregando modelo clienteverificacion

---
+++ app/Models/ClienteVerificacion.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ClienteVerificacion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cliente_verificacion';
+    protected $fillable = ['fecha_expiracion', 'codigo'];
+
+    public function cliente(): BelongsTo
+    {
+        return $this -> belongsTo(Cliente::class);
+    }
+}
