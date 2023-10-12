0a1fb2eb98970b14582fd9a8400fc1d25dd6255c
Mercedes Wyss <itrjwyss@umes.edu.gt>
2023-10-12 00:05:38 -0600

    Desplegando listado de sucursales

--- resources/views/console/sucursales/sucursales/list.blade.php
+++ resources/views/console/sucursales/sucursales/list.blade.php
@@ -4,4 +4,27 @@
 
 @section('content')
     <h1>Listado de Sucursales</h1>
+
+    <br>
+
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Dirección</th>
+            <th>Referencias</th>
+            <th>Teléfonos</th>
+            <th>Whatsapp</th>
+            <th>Estado</th>
+        </tr>
+        @foreach ($sucursales as $sucursal)
+            <tr>
+                <td>{{ $sucursal -> nombre }}</td>
+                <td>{{ $sucursal -> direccion }}</td>
+                <td>{{ $sucursal -> referencias }}</td>
+                <td>{{ $sucursal -> telefonos }}</td>
+                <td>{{ $sucursal -> whatsapp }}</td>
+                <td>{{ $sucursal -> getEstado() }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
