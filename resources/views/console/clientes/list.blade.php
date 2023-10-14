d6145bbcac7735aa2400c008ac8152fffb39c643
keithre19 <ivana2003@umes.edu.gt>
2023-10-14 10:06:04 -0600

    Listado de Clientes

--- resources/views/console/clientes/list.blade.php
+++ resources/views/console/clientes/list.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.console')
 
-@section('title', 'Sucursales')
+@section('title', 'Clientes')
 
 @section('content')
-    <h1>Listado de Sucursales</h1>
+    <h1>Listado de Clientes</h1>
+
+<a href="/clientes/create">Crear Cliente</a>
+
+    <br><br>
+
+    <table>
+        <tr>
+            <th>Nombres</th>
+            <th>Recibir Noticias</th>
+            <th>Recibir Promociones</th>
+            <th>Recibir Categoria Favorita</th>
+            <th>Recibir lavoratorio favorito</th>
+            <th>Recibir producto favorito</th>
+            <th>Verificado</th>
+            <th>Estado</th>
+        </tr>
+        @foreach ($clientes as $cliente)
+            <tr>
+                <td>{{ $cliente -> nombres }}</td>
+                <td>{{ $cliente -> getRecibirNoticias() }}</td>
+                <td>{{ $cliente -> getRecibirPromociones() }}</td>
+                <td>{{ $cliente -> getRecibirFavCategoria() }}</td>
+                <td>{{ $cliente -> getRecibirFavLaboratorio() }}</td>
+                <td>{{ $cliente -> getRecibirFavProducto() }}</td>	
+                <td>{{ $cliente -> getVerificado() }}</td>	
+                <td>{{ $cliente -> getEstado() }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
