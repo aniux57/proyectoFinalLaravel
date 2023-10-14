c0140f97e684a6e6866db7ac36db83bc876dfbfd
keithre19 <ivana2003@umes.edu.gt>
2023-10-13 18:33:59 -0600

    Listado de Municipios

--- app/Http/Controllers/console/sucursales/MunicipiosController.php
+++ app/Http/Controllers/console/sucursales/MunicipiosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\console\sucursales;
 
 use App\Http\Controllers\Controller;
+use App\Models\Municipio;
 use Illuminate\Http\Request;
 
 class MunicipiosController extends Controller
@@ -10,7 +11,10 @@ class MunicipiosController extends Controller
 
     public function index()
     {
-        //
+        return view(
+            'console/sucursales/municipios/list',
+            ['municipios' => Municipio::all()]
+        );
     }
 
     public function create()
--- resources/views/console/sucursales/municipios/list.blade.php
+++ resources/views/console/sucursales/municipios/list.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.console')
 
-@section('title', 'Sucursales')
+@section('title', 'Municipios')
 
 @section('content')
-    <h1>Listado de Sucursales</h1>
+    <h1>Listado de Municipios</h1>
+
+    <a href="/municipios/create">Crear Departamento</a>
+
+    <br><br>
+
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Departamento</th>
+            <th>Estado</th>
+        </tr>
+        @foreach ($municipios as $municipio)
+            <tr>
+                <td>{{ $municipio -> nombre }}</td>
+                <td>{{ $municipio -> departamento -> nombre }}</td>
+                <td>{{ $municipio -> getEstado() }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
