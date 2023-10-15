eae96f68dad648ec8fa3ce9742610787a702b372
keithre19 <ivana2003@umes.edu.gt>
2023-10-14 21:47:57 -0600

    Creacion Tipo Promocion

--- app/Http/Controllers/console/promociones/TiposPromocionesController.php
+++ app/Http/Controllers/console/promociones/TiposPromocionesController.php
@@ -19,12 +19,20 @@ class TiposPromocionesController extends Controller
 
     public function create()
     {
-        //
+        return view('console/promociones/tiposPromociones/create');
     }
 
     public function store(Request $request)
     {
-        //
+        $tipo_promocion = new tipoPromocion();
+        $tipo_promocion -> nombre = $request->input('nombre');
+        $tipo_promocion -> descripcion = $request->input('descripcion');
+        $tipo_promocion -> unidades = $request->input('unidades');
+        $tipo_promocion -> descuento = $request->input('descuento');
+        $tipo_promocion -> estado = true;
+        $tipo_promocion -> save();
+
+        return redirect()->route('tiposPromociones.index');
     }
 
     public function edit(string $id)
--- resources/views/console/promociones/tiposPromociones/create.blade.php
+++ resources/views/console/promociones/tiposPromociones/create.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.console')
 
-@section('title', 'Sucursales')
+@section('title', 'Tipos de Promociones')
 
 @section('content')
-<h1>Creación de Sucursal</h1>
+    <h1>Creación de Tipos de Promociones</h1>
+
+    <form method="post" action="/tiposPromociones">
+        @csrf
+        <input type="text" id="nombre" name="nombre" placeholder="Nombre">
+        <input type="text" id="descripcion" name="descripcion" placeholder="Descripcion">
+        <input type="text" id="unidades" name="unidades" placeholder="Unidades">
+        <input type="text" id="descuento" name="descuento" placeholder="Descuento">
+
+        <input type="submit" value="Crear">
+    </form>
 @endsection
