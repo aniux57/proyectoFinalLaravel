76fd0d32136e3a5ecca1efa5d59ee08f89f13b63
keithre19 <ivana2003@umes.edu.gt>
2023-10-15 02:27:12 -0600

    Creacion promociones

--- app/Http/Controllers/console/promociones/PromocionesController.php
+++ app/Http/Controllers/console/promociones/PromocionesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\console\promociones;
 
 use App\Http\Controllers\Controller;
+use App\Models\Producto;
 use App\Models\Promocion;
+use App\Models\TipoPromocion;
 use Illuminate\Http\Request;
 
 class PromocionesController extends Controller
@@ -19,12 +21,25 @@ class PromocionesController extends Controller
 
     public function create()
     {
-        //
+        return view(
+            'console/promociones/Promociones/create',
+            ['productos' => Producto::all()],
+            ['tiposPromociones' => TipoPromocion::all()]
+        );
     }
 
     public function store(Request $request)
     {
-        //
+        $promocion = new Promocion();
+        $promocion -> nombre = $request -> input('nombre');
+        $promocion -> fecha_inicio = $request -> input('fecha_inicio');
+        $promocion -> fecha_fin = $request -> input('fecha_fin');
+        $promocion -> id_tipo_promocion = $request -> input('id_tipo_promocion');
+        $promocion -> id_producto = $request -> input('id_producto');
+        $promocion -> estado = true;
+        $promocion -> save();
+
+        return redirect() -> route('promociones.index');
     }
 
     public function edit(string $id)
