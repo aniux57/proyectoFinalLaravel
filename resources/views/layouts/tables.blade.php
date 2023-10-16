32b146d557bac72b320ab1b5ceb64c7a8c9bfbd6
Mercedes Wyss <itrjwyss@umes.edu.gt>
2023-10-15 19:12:19 -0600

    Agregando template faltante

--- resources/views/layouts/tables.blade.php
+++ resources/views/layouts/tables.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts.console')
+
+@section('styles')
+<link href="{!! asset('matrix/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css') !!}" rel="stylesheet" type="text/css" />
+@endsection
+
+@section('scripts')
+<script src="{!! asset('matrix/assets/extra-libs/multicheck/datatable-checkbox-init.js') !!}"></script>
+<script src="{!! asset('matrix/assets/extra-libs/multicheck/jquery.multicheck.js') !!}"></script>
+<script src="{!! asset('matrix/assets/extra-libs/DataTables/datatables.min.js') !!}"></script>
+<script>
+    $('#zero_config').DataTable();
+</script>
+@endsection
